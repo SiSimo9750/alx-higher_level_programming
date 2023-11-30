fc6393b419c61835c2f240e110b8b27382d94694
SiSimo9750 <bourhim.mo8@gmail.com>
2023-11-30 14:57:16 -0800

    coe debugging for task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    res1 = add(a,b)
-    res2 = sub(a,b)
-    res3 = mul(a,b)
-    res4 = div(a,b)
-    print("{} + {} = {}".format(a, b, res1))
-    print("{} - {} = {}".format(a, b, res2))
-    print("{} * {} = {}".format(a, b, res3))
-    print("{} / {} = {}".format(a, b, res4))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
