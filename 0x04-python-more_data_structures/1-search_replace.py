e72e8a90ec3473ef0a3d17d461ca1c987199fd36
SiSimo9750 <bourhim.mo8@gmail.com>
2023-12-06 17:29:08 -0800

    debugging my codes

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,3 +4,4 @@ def search_replace(my_list, search, replace):
     for i in range(len(my_new_list)):
         if my_new_list[i] == search:
             my_new_list[i] = replace
+    return my_new_list
