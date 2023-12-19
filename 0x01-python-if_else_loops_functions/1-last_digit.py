0b1fe697a08a781c46f82b421688811aeb6bc5a6
SiSimo9750 <bourhim.mo8@gmail.com>
2023-12-18 17:40:13 -0800

    Modifiying codes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
+last_dig = abs(number)%10
 if number < 0:
-    new_number = number * -1
-else:
-    new_number = number
-last_dig = new_number % 10
+    last_dig = last_dig * (-1)
 if last_dig > 5:
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
 elif last_dig < 6 and last_dig != 0:
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("Fizzbuzz ", end="")
         elif i % 3 == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz ", end="")
         else:
-            print(i, end="")
+            print("{} ".format(i), end="")
