86f3ad2e279e5085d3a1d70953270c7522155c6f
SiSimo9750 <bourhim.mo8@gmail.com>
2024-01-09 17:24:16 -0800

    task7 again

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Generate list from arguments and save to a file."""
+
+
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        generatedList = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        generatedList = []
-    generatedList.extend(sys.argv[1:])
-    save_to_json_file(generatedList, "add_item.json")
+try:
+    extractedList = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    extractedList = []
+extractedList.extend(sys.argv[1:])
+save_to_json_file(extractedList, "add_item.json")
