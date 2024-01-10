a6a042f524e2daf25e887cf4a01921cd5cc6da7f
SiSimo9750 <bourhim.mo8@gmail.com>
2024-01-09 17:13:27 -0800

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
+# 7-add_item.py
 #!/usr/bin/python3
-"""Generate a Python list from arguments and save to a file."""
-
-
+"""Generate list from arguments and save to a file."""
 import sys
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        items = load_from_json_file("add_item.json")
+        generatedList = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        generatedList = []
+    generatedList.extend(sys.argv[1:])
+    save_to_json_file(generatedList, "add_item.json")
