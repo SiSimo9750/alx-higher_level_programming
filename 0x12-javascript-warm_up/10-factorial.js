65bd5d377fda9ba25f89710ad5ea54cce3cebe05
SiSimo9750 <bourhim.mo8@gmail.com>
2024-03-21 15:42:55 -0700

    Debugging task 8 and 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,9 @@
 function factorial (n) {
   if (n === 0 || isNaN(n)) {
     return (1);
-  } else
-	return (n * factorial(n - 1));
+  } else {
+    return (n * factorial(n - 1));
+  }
 }
 
 console.log(factorial(Number(process.argv[2])));
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   const x = Number(process.argv[2]);
-  
-  for (let i = 0 ; i < x; i++) {
+
+  for (let i = 0; i < x; i++) {
     console.log('X'.repeat(x));
   }
 }
