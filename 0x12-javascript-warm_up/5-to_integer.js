762f27f6839d3dab33038c3712a5a482af965454
SiSimo9750 <bourhim.mo8@gmail.com>
2024-03-21 15:48:28 -0700

    Debugging task 5 and 7

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const MyNumber = Math.floor(Number(process.argv[2]));
-console.log(isNaN(MyNumber) ? 'Not a number' : `My number: ${num}`);
+console.log(isNaN(MyNumber) ? 'Not a number' : `My number: ${MyNumber}`);
