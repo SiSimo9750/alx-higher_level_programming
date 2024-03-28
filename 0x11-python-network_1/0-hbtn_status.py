3b8119ca9d995d5cad3ce94b2226278c00015cef
SiSimo9750 <bourhim.mo8@gmail.com>
2024-03-28 16:41:10 -0700

    pycodestyle check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as respo:
-        RespoContent = respo.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        RespoContent = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(RespoContent)))
         print("\t- content: {}".format(RespoContent))
