2a2ca6afd8e4f0f64814a8daec063b407b66e51a
SiSimo9750 <bourhim.mo8@gmail.com>
2024-03-28 17:48:55 -0700

    more task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    myReq = requests.get("https://intranet.hbtn.io/status")
+    myReq = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(myReq.text)))
     print("\t- content: {}".format(myReq.text))
